56076ca55e3e7eb1e42be1f34e45a47d1be83204
forgitd <1826671475@qq.com>
2021-05-21 09:02:09 +0800

    文件目录组织

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import actions from "@/store/actions";
+import mutations from "@/store/mutations";
+import getters from "@/store/getters";
+import moduleA from "@/store/modules/moduleA";
+
 Vue.use(Vuex)
 
+const a = moduleA
 export default new Vuex.Store({
   state: {
   },
-  mutations: {
-  },
-  actions: {
-  },
+  mutations,
+  actions,
+  getters,
   modules: {
+    a
   }
 })
